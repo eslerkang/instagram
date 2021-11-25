eac3995ce2fa4cc8c4d68364928398309bb55632
EslerKang <eslerkang@gmail.com>
2021-11-25 21:32:39 +0900

    added users.models/contact-unique

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ class User(TimeStampModel):
     name     = models.CharField(max_length=15)
     email    = models.CharField(max_length=254, unique=True)
     password = models.CharField(max_length=200)
-    contact  = models.CharField(max_length=19)
+    contact  = models.CharField(max_length=19, unique=True)
     mbti     = models.CharField(max_length=4, blank=True)
     gender   = models.CharField(max_length=9)
 
