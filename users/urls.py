f4a15cfbddb82c2e91e669af7f1c1ce7cdcbe53f
EslerKang <eslerkang@gmail.com>
2021-11-25 17:44:32 +0900

    creating SignupView

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
-urlpatterns = [
+from users.views import SignupView
 
+urlpatterns = [
+    path('/signup', SignupView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+import bcrypt
+from django.views import View
+from django.http  import JsonResponse
+
+from users.models import User
+
+class SignupView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            name = data['name']
+            email = data['email']
+            password = data['password']
+            contact = data['contact']
+            mbti = data.get('mbti', None)
+            gender = data['gender']
+
+            hashed_password = bcrypt.hashpw(
+                password.encode('utf-8'),
+                bcrypt.gensalt()
+            ).decode('utf-8')
+
+            user = User()
+
+            return JsonResponse({'MESSAGE': 'CREATED'}, status=201)
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
