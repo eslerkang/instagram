f8ed4f4bf73732af5464bce328ce3c48dc6e55bc
EslerKang <eslerkang@gmail.com>
2021-11-28 19:12:16 +0900

    MODIFIED, CREATED / modified follow model, created follow View

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import SignupView, SigninView
+from users.views import SignupView, SigninView, FollowView
 
 urlpatterns = [
     path('/signup', SignupView.as_view()),
     path('/signin', SigninView.as_view()),
+    path('/follow', FollowView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -4,9 +4,10 @@ import bcrypt, jwt
 from django.views           import View
 from django.http            import JsonResponse
 from django.core.exceptions import ValidationError
+from django.db.models       import F
 
 from my_settings            import SECRET_KEY, ALGORITHM
-from users.models           import User
+from users.models           import User, Follow
 from core.validations       import (
     validate_email,
     validate_password,
@@ -15,6 +16,8 @@ from core.validations       import (
     validate_mbti,
     validate_gender
 )
+from core.utils             import authorization
+
 
 class SignupView(View):
     def post(self, request):
@@ -108,3 +111,59 @@ class SigninView(View):
 
         except User.DoesNotExist:
             return JsonResponse({'MESSAGE': 'INVALID_EMAIL'}, status=401)
+
+
+class FollowView(View):
+    @authorization
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user = request.user
+
+            user_id = data['user_id']
+
+            following = User.objects.get(id=user_id)
+
+            if user.id == following.id:
+                return JsonResponse({'MESSAGE': 'CANNOT_FOLLOW_YOURSELF'}, status=400)
+
+            follow = Follow.objects.get_or_create(follower=user, following=following)
+
+            if not follow[1]:
+                follow[0].delete()
+                return JsonResponse({'MESSAGE': 'FOLLOW_CANCELED_SUCCESS'}, status=200)
+
+            return JsonResponse({'MESSAGE': 'FOLLOW_SUCCESS'}, status=201)
+
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'MESSAGE': 'BODY_REQUIRED'}, status=400)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+        except ValueError:
+            return JsonResponse({'MESSAGE': 'INVALID_USER_ID'}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE': 'USER_NOT_FOUND'}, status=400)
+
+    @authorization
+    def get(self, request):
+        user      = request.user
+        follower  = Follow.objects.filter(following=user).select_related('follower').annotate(
+            name=F('follower__name')
+        )
+        following = Follow.objects.filter(follower=user).select_related('following').annotate(
+            name=F('following__name')
+        )
+
+        result = {
+            "name"       : user.name,
+            "created_at" : user.created_at,
+            "follower_count": follower.count(),
+            "follower"   : list(follower.values('name')),
+            "following_count": following.count(),
+            "following"  : list(following.values('name')),
+        }
+
+        return JsonResponse({'MESSAGE': result}, status=200)
